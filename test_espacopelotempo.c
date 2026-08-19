06f39410cc0b10441efc221a6b9c382d6f9dd8de
Pietra13015 <pietra7242@gmail.com>
2026-08-18 22:15:11 -0300

    Add files via upload

---
+++ test_espacopelotempo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include "function_espacopelotempo.c"
+
+int main(void) {
+    assert(fabs(espaco(80, 2, 0) - 160.0) < 0.01);
+    assert(fabs(espaco(40, 2, 0) - 80.0) < 0.01);
+    assert(fabs(espaco(40, 2, 3) - 86.0) < 0.01);
+
+    printf("Todos os testes passaram!\n");
+    printf("Criado por Pietra Oliveira Narciso Santos");
+
+    return 0;
+}
